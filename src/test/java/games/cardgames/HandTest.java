6067e25e6aaebc843859ebe83ef6a6455a1cdcfd
AmandaMWilliams <73121365+AmandaMWilliams@users.noreply.github.com>
2020-11-20 07:49:35 -0500

    added HandTest

--- src/test/java/games/cardgames/HandTest.java
+++ src/test/java/games/cardgames/HandTest.java
@@ -1,21 +1,38 @@
 package games.cardgames;
 
-//AMANDA'S WORKING HERE DO NOT TOUCH//
 
-//
-//import org.junit.Test;
-//
-//import java.util.ArrayList;
-//
-//public class HandTest {
-//    @Test
-//    public void getHandTest(){
-//        //given
-//        Hand hand = new Hand();
-//        ArrayList<Card> expectedArray =
-//        //when
-//        //then
-//    }
-//
-//
-//}
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class HandTest {
+
+    @Test
+    public void getHandTest(){
+        //given
+        Hand hand = new Hand();
+        ArrayList<Card> expectedHand = new ArrayList<>();
+        //when
+        hand.setHand(expectedHand);
+        ArrayList<Card> actualHand = hand.getHand();
+        //then
+        Assert.assertEquals(expectedHand, actualHand);
+    }
+
+    @Test
+    public void getHandSizeTest(){
+        //given
+        Hand hand = new Hand();
+        int expectedHandSize = 0;
+        //when
+        int actualHandSize = hand.getHandSize();
+        //then
+        Assert.assertEquals(expectedHandSize, actualHandSize);
+    }
+
+
+
+
+
+}
