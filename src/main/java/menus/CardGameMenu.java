95fe60d437c500b649e6f6a859f957340235079a
kppetrick <kppetrick@gmail.com>
2020-11-19 23:14:24 -0500

    sorta close to menu done

--- src/main/java/menus/CardGameMenu.java
+++ src/main/java/menus/CardGameMenu.java
@@ -1,24 +1,21 @@
 package menus;
-
-
+import games.cardgames.BlackjackGame;
 import games.cardgames.GoFish;
 import io.zipcoder.casino.utilities.Console;
+import player.BlackjackPlayer;
+import player.FatCatDealer.BlackjackDealer;
 import player.FatCatDealer.GoFishDealer;
 import player.GoFishPlayer;
 import player.Player;
-
 public class CardGameMenu {
     Console console;
     Player player;
-
     public CardGameMenu(Console console, Player player) {
         this.console = console;
         this.player = player;
     }
-
     public String runCardGameMenu() {
         boolean cardGameMenuPower = true;
-        //TODO: Add ASCII ART
         Integer selection = 0;
         while (cardGameMenuPower) {
             System.out.println("\n" +
@@ -29,38 +26,30 @@ public class CardGameMenu {
                     "---------------------------------\n");
             selection = console.getIntegerInput("Enter choice here: -> ");
             runCardGameSwitch(selection);
-
         }
         return String.valueOf(selection);
     }
-
     public void runCardGameSwitch(Integer selection) {
         switch (selection) {
             case 1:
-                //Play Blackjack
-//                BlackjackGame game = new BlackjackGame();
-//                        game.run
+                BlackjackGame blackjackGame = new BlackjackGame(new BlackjackPlayer(console, player.getUsername(), player.getPassword(), player.getPlayersAccount()),new BlackjackDealer(), console);
+                blackjackGame.startGame();
                 break;
             case 2:
                 GoFish goFishGame= new GoFish(new GoFishPlayer(console, player.getUsername(), player.getPassword(), player.getPlayersAccount()),new GoFishDealer(), console);
+                goFishGame.startGame();
                 break;
             case 3:
                 //run main menu
                 break;
             default:
-
                 break;
         }
-
     }
-
-
     public String runBlackjack() {
         return null;
     }
-
     public String runGoFish() {
         return null;
     }
-
 }
